cf947e2a2b44cf242f8cf74c98e97bdcb01a9dac
Maciej Gajewski <gajewski.mm@gmail.com>
2012-10-13 16:30:06 +0200

    Imgkit path fix

--- config/imgkit.rb
+++ config/imgkit.rb
@@ -1,5 +1,5 @@
 IMGKit.configure do |config|
-  config.wkhtmltoimage = Rails.root.join('bin', 'wkhtmltoimage').to_s if ENV['RACK_ENV'] == 'production'
+  config.wkhtmltoimage = File.expand_path File.dirname(__FILE__) + '/bin/wkhtmltoimage'  if ENV['RACK_ENV'] == 'production'
   config.default_options = { :quality => 60 }
   config.default_format  = :png
 end

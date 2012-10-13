1c7fd7919ca7cb310f26c850167f77e6e7f5f68c
dariusz gorzeba <godot@tlen.pl>
2012-10-13 19:46:18 +0000

    tip

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,4 +8,27 @@ class Event
 
   field :action, type: Hash # it's an user
   field :id  
+
+
+  def self.by_user(login, page = 1)
+    src = Octokit.user_events(login, :page => page)
+
+    src.map do |event_attributes|
+      Event.new(event_attributes)
+    end
+  end
+
+  def self.all_by_user(login)
+    # we are able to rich 300 event from gh
+    # default per_page = 30
+    # so 
+    events = []
+    
+    (1..10).times do |i|
+      events + by_user(login,i)
+    end
+
+    events
+
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require './init'
 require 'rack/test'
+require 'vcr'
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
@@ -7,4 +8,11 @@ RSpec.configure do |config|
   config.filter_run :focus
 
   config.order = 'random'
+  config.extend VCR::RSpec::Macros
+end
+
+VCR.config do |c|
+  c.cassette_library_dir     = 'spec/cassettes'
+  c.stub_with                  :fakeweb
+  c.default_cassette_options = { :record => :new_episodes }
 end

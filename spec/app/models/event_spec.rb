d9ccbe33fd7343d30e76c0eb7a60ae6386d891bf
dariusz gorzeba <dgorzeba@gmail.com>
2012-10-14 12:04:21 +0200

    repo data fetching tests

--- spec/app/models/event_spec.rb
+++ spec/app/models/event_spec.rb
@@ -3,10 +3,6 @@ require 'spec_helper'
 describe Events do
   use_vcr_cassette :events
 
-  before do
-    Repo.stub! lookup: Repo.new(fullname: "stubbed-repo")
-  end
-
   it ' connects to github and fetches 300 records ' do
     response = Events.for('godot')
     response.size.should <= 300

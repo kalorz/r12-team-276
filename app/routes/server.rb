4004f35b1f409b06ae5c5ad8a3a95455e9a70ed3
Karol Sarnacki <sodercober@gmail.com>
2012-10-14 17:40:46 +0200

    Fix routes for main page/signature

--- app/routes/server.rb
+++ app/routes/server.rb
@@ -1,23 +1,25 @@
 class Main
 
-  get /\A\/([^.]+)?(\.png)?\z/ do |login, format|
-    login  ||= params['login']
-    format ||= params['format']
+  # Get login from URL or parameter (/?login=xxx)
+  get '/:login?' do
+    return slim(:index, layout: !request.xhr?) unless params[:login]
 
-    @current_user = begin
-      User.find_or_create_by(login: login)
-    rescue
-      raise Sinatra::NotFound
+    if params[:login].index('.')
+      @login, _, @format = params[:login].rpartition('.')
+    else
+      @login = params[:login]
     end
 
-    if format == '.png'
+    @current_user = User.find_or_create_by(login: @login)
+
+    if @format == 'png'
       user       = User.new
       user.login = 'ksarnacki'
       user.score = 1000
       badge      = Badge.new(user)
-      badge.render(true, "public/img/badges/#{user.login}.png")
-      @login = user.login
-      slim :badge_display
+      file_name  = "public/img/badges/#{user.login}.png"
+      badge.render(true, file_name)
+      send_file(file_name, disposition: 'inline', type: 'image/png', filename: File.basename(file_name))
     else
       slim :index, layout: !request.xhr?
     end

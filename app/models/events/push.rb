e2d7db74bbeda87330809cd2826ce0fa61cb8553
dariusz gorzeba <dgorzeba@gmail.com>
2012-10-14 10:01:26 +0200

    refactoring

--- app/models/events/push.rb
+++ app/models/events/push.rb
@@ -1,11 +1,12 @@
-class Events::Push
-  def initialize(event_data)
-    @repo = Repo.find(event_data['repo']['name'])
-    @commit_count = event_data['payload']['size']
-  end
+module Events
+  class Push
+    def initialize(event_data)
+      @repo = Repo.find(event_data['repo']['name'])
+      @commit_count = event_data['payload']['size']
+    end
 
-  def rank
-    @repo.rank * @commit_count
+    def rank
+      @repo.rank * @commit_count
+    end
   end
 end
-

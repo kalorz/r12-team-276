fbdd79f5d15a51240a456e263b54165f70a901fb
samuil <samuil@o2.pl>
2012-10-14 19:33:41 +0200

    switched deployment to master

--- config/deploy.rb
+++ config/deploy.rb
@@ -61,7 +61,7 @@ ssh_options[:keys] = ["~/.ssh/id_rsa"]
 # SCM Options
 set :scm,        :git
 set :repository, "git@github.com:railsrumble/#{GITHUB_REPOSITORY_NAME}.git"
-set :branch,     "dev"
+set :branch,     "master"
 
 # Roles
 role :app, LINODE_SERVER_HOSTNAME
